d35e33c26254043603aff5ea6f7c0cc716b98164
Laura <lllaralaura7@gmail.com>
2020-09-18 22:02:33 -0500

    Getting AWS ready

---
+++ src/build/precache-manifest.5274dea7703d88eba43be759d4ff81f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105ffc3b2cd2a8653ebaa36a334f1f18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "977f5a550a791518f03f",
+    "url": "/static/css/main.761f8ad9.chunk.css"
+  },
+  {
+    "revision": "b4b7b9935238a26ed566",
+    "url": "/static/js/2.193da213.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.193da213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977f5a550a791518f03f",
+    "url": "/static/js/main.6b30d986.chunk.js"
+  },
+  {
+    "revision": "d72424798ea2e40f89ab",
+    "url": "/static/js/runtime-main.8c1f49b6.js"
+  }
+]);
